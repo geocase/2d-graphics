bb28c05bfb7de821343f921fd9495c3f655a3d55
Casey Georgeson <caseys.georgeson@gmail.com>
2022-04-03 11:16:56 -0700

    Apply formatting
    Remember to do this before committing!

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@ int main() {
 		printf("L\n");
 		exit(-1);
 	}
-	
+
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -109,10 +109,10 @@ int main() {
 	char *screen_shader_vs = readFilePathToCStr("shaders/buffer.vs");
 	char *screen_shader_fs = readFilePathToCStr("shaders/buffer.fs");
 	Shader_t screen_shader = shdNewShader(screen_shader_vs, screen_shader_fs);
-	
+
 	rGenerateFrameBuffer(&renderer, (vec2){BUFFER_X, BUFFER_Y}, FB_SCENE,
 						 screen_shader);
-	 
+
 	struct LightMesh point3;
 
 	Game_t game = {
@@ -235,7 +235,7 @@ int main() {
 		game.actors[0].position[0] -= adjust[0] * 2;
 
 		rSwapFrameBuffer(&renderer, FB_SCENE);
-		
+
 		rClear(&renderer);
 		mat4 model;
 		glm_mat4_identity(model);
